6a39d20cf3cf89281c298767b234e0049d5a7825
ChilliD <87444703+ChilliD@users.noreply.github.com>
2021-09-30 03:01:48 -0400

    news-tab
    added news tab and API
    DOM clears before loading new content
    added error message if content fails to load

--- scripts.js
+++ scripts.js
@@ -1,20 +1,57 @@
 url = 'https://api.coincap.io/v2/assets';
-newsUrl = '';
-newsAPIKEY = '613f716142734cab90eb32e2dd57692f';
+newsUrl = 'https://min-api.cryptocompare.com/data/v2/news/?lang=EN&api_key=f765105d033d75bf98e339c6a007f9df7044f770d08efe106e6df4398926740a&sortOrder=popular';
 const req = new XMLHttpRequest();
 
 let dataset = [];
 let values = [];
 let icons = [];
+let news;
+let articles = [];
 
 const container = document.getElementById('content');
 
 const formatNum = new Intl.NumberFormat(undefined, { minimumFractionDigits: 3 });
 
+//Error Handling
+function reloadAPI() {
+    location.reload();
+    return false;
+}
 
+function handleLoadError() {
+    let errorMsg = document.createElement('div');
+    errorMsg.classList.add('error-box');
+    errorMsg.innerHTML = 
+        `<span class="error-text">Failed to load</span><br />
+        <button class="error-btn" onclick="reloadAPI()">Reload <i class="fas fa-redo"></i></button>
+        `;
+    container.appendChild(errorMsg);
+}
 
+//Content
 function drawBoxes() {
-values.forEach(val => createCard(val));
+    clearContainer();
+    values.forEach(val => createCard(val));
+}
+
+function drawNews() {
+    clearContainer();
+    let targetArticles = articles.slice(0, 9);
+    targetArticles.forEach(article => {
+        let articleCard = document.createElement('div');
+        let articleDate = new Date(1000 * article.published_on);
+        articleCard.classList.add('article-card');
+        articleCard.innerHTML = 
+            `<img class="article-img" src="${article.imageurl}"></img>
+            <div class="article-text-box">
+            <span class="article-title">${article.title}</span> <br />
+            <div class="article-bottom">
+            <span class="article-date">${articleDate.toLocaleDateString()}</span>
+            <span class="article-source">Source: <a href="${article.url}" target="_blank">${article.source}</a></span>
+            </div></div>
+            `;
+        container.appendChild(articleCard);
+    })
 }
 
 function createCard(coin) {
@@ -29,7 +66,9 @@ function createCard(coin) {
         else { adjustedColor = '#2c3e50' };
     coinBox.classList.add('coin-box');
     coinBox.innerHTML = 
-        `<div class="head-span"><i><img src="${iconUrl}" class="coin-icon"></i>${coin.symbol}</div>
+        `<div class="head-span"><i><img src="${iconUrl}" class="coin-icon"></i>
+            <span class="title-wrap"><span class="coin-title">${coin.name}</span><br />${coin.symbol}</span>
+        </div>
         <div class="numbers-wrap">
         <span class="price">$${formattedPrice}</span>
         <span class="percent-change" style="color:${adjustedColor}">${formattedChange}%</span>
@@ -39,10 +78,31 @@ function createCard(coin) {
 
 }
 
+function clearContainer() {
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+}
+
+
+//API Calls
 req.open('GET', url);
+req.onerror = () => {
+    handleLoadError();
+};
 req.onload = () => {
-  dataset = JSON.parse(req.responseText);
-  values = dataset.data;
-  drawBoxes();
+    dataset = JSON.parse(req.responseText);
+    values = dataset.data;
+    drawBoxes();
 };
 req.send();
+
+function getNews() {
+    req.open('GET', newsUrl);
+    req.onload = () => {
+        news = JSON.parse(req.responseText);
+        articles = news.Data;
+        drawNews();
+    };
+    req.send();
+}
